da488aba549fd1f10036bd7de6525db32a4b39e5
Robot <colepearson27@proton.me>
2025-03-01 12:16:37 -0800

    Change to make slow mode have priority.
     Changes to be committed:

--- src/main/java/Team4450/Robot25/subsystems/DriveBase.java
+++ src/main/java/Team4450/Robot25/subsystems/DriveBase.java
@@ -123,6 +123,8 @@ public class DriveBase extends SubsystemBase {
   private SlewRateLimiter rotLimiter = new SlewRateLimiter(DriveConstants.kRotationalSlewRate);
   private double prevTime = WPIUtilJNI.now() * 1e-6;
 
+  private boolean slowModeEnabled = false;
+
   // Odometry class for tracking robot pose
   // SwerveDriveOdometry odometry = new SwerveDriveOdometry(
   //     DriveConstants.kDriveKinematics,
@@ -862,6 +864,7 @@ public class DriveBase extends SubsystemBase {
    */
   public void enableSlowMode()
   {
+    slowModeEnabled = true;
     speedLimiter = DriveConstants.kSlowModeFactor;
     rotSpeedLimiter = DriveConstants.kRotSlowModeFactor;
 
@@ -875,6 +878,7 @@ public class DriveBase extends SubsystemBase {
    */
   public void disableSlowMode()
   {
+    slowModeEnabled = false;
     Util.consoleLog();
 
     speedLimiter = 1;
@@ -888,12 +892,12 @@ public class DriveBase extends SubsystemBase {
    */
   public void setElevatorHeightSpeed(double height)
   {
-    speedLimiter = Math.pow(2, -(1.2 * height));
-    rotSpeedLimiter = Math.pow(2, -(1.2 * height)) + 0.2;
-
-    Util.consoleLog("%.2f %.2f", speedLimiter, rotSpeedLimiter);
-
-    updateDS();
+    if (!slowModeEnabled) {
+        speedLimiter = Math.pow(2, -(1.2 * height));
+        rotSpeedLimiter = Math.pow(2, -(1.2 * height)) + 0.2;
+        Util.consoleLog("%.2f %.2f", speedLimiter, rotSpeedLimiter);
+        updateDS();
+    }
   }
 
 public void enableTrackingSlowMode(){
