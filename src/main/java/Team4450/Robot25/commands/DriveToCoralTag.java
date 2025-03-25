a48ffdedff7f483f42320cd122fc225848d11951
Srivikaat (Vikaat) Ravi <vikaat08@gmail.com>
2025-03-25 16:16:12 -0700

    Update DriveToCoralTag.java

--- src/main/java/Team4450/Robot25/commands/DriveToCoralTag.java
+++ src/main/java/Team4450/Robot25/commands/DriveToCoralTag.java
@@ -19,7 +19,7 @@ import Team4450.Robot25.subsystems.DriveBase;
  */
 
 public class DriveToCoralTag extends Command {
-    PIDController rotationController = new PIDController(0.03, 0.001, 0); // for rotating drivebase
+    PIDController rotationController = new PIDController(0.04, 0.001, 0); // for rotating drivebase
     PIDController translationController = new PIDController(0.05, 0.005, 0); // for moving drivebase in X,Y plane
     DriveBase robotDrive;
     PhotonVision photonVision;
